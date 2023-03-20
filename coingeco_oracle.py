ef2ff4aa6e92432a51e6cd4d5332c0dbea67fb7c
song sjun <song.sjun@hotmail.com>
2023-03-21 03:13:29 +0800

    support busd

--- coingeco_oracle.py
+++ coingeco_oracle.py
@@ -11,7 +11,7 @@ def fetch_prices():
     url = "https://api.coingecko.com/api/v3/coins/markets"
     params = {
         "vs_currency": "usd",
-        "ids": "bitcoin,ethereum,usd-coin"
+        "ids": "bitcoin,ethereum,usd-coin,binance-usd"
     }
     response = requests.get(url, params=params)
     response.raise_for_status()
